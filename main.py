e02387dd23a3107a097051e995a4adddf5621a72
tstinghen <tstinghen@gmail.com>
2015-04-22 09:19:02 -0700

    Created Business.py
    Added functionality for getting and posting Businesses

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ application = webapp2.WSGIApplication([
 
 	(r'/category', 'category.Category'),
 	(r'/item', 'item.Item'),
+	(r'/business', 'business.Business'),
 	(r'/', 'front.Front'), 
 ], debug=True)
  
